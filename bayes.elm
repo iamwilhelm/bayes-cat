fde66d0cd3e4c1e09cff5cbe0c49ac2cb92f5718
Wil Chung <iamwil@gmail.com>
2016-03-13 01:11:00 -0800

    added multiple entity types

--- bayes.elm
+++ bayes.elm
@@ -6,16 +6,28 @@ import Graphics.Element exposing (..)
 import Window
 import Mouse
 import Time exposing (..)
+import List
 
 view : (Int, Int) -> App -> Element
 view (width, height) app =
   collage width height
-    [ move (app.x, app.y) kitty
-    ]
+  <| List.map viewEntity app.entities
+
+viewEntity : Entity -> Form
+viewEntity entity =
+  case entity of
+    Cursor object ->
+      move (object.pos.x, object.pos.y) kitty
+    Turtle object ->
+      move (object.pos.x, object.pos.y) doggy
 
 kitty : Form
 kitty =
   traced (solid blue) square
+
+doggy : Form
+doggy =
+  traced (solid red) square
 
 square : Path
 square =
@@ -26,22 +38,47 @@ square =
 update : Input -> App -> App
 update input app =
   { app |
-    x = fst input.mouse
-  , y = snd input.mouse
+    entities = List.map (updateEntity input) app.entities
   }
+
+updateEntity : Input -> Entity -> Entity
+updateEntity input entity =
+  case entity of
+    Cursor object ->
+      Cursor { object |
+        pos = { x = fst input.mouse, y = snd input.mouse }
+      }
+    Turtle object ->
+      Turtle { object |
+        pos = { x = object.pos.x, y = object.pos.y - 2 }
+      }
 
 -------------- Model methods
 
 type alias App =
-  { x : Float
-  , y : Float
+  { entities: List Entity
   }
 
 initApp : App
 initApp = {
-    x = 0.0
-  , y = 0.0
+    entities = [
+      Cursor { pos = { x = 0.0, y = 0.0 } }
+    , Turtle { pos = { x = 0.0, y = 0.0 } }
+    ]
   }
+
+type Entity =
+  Cursor Object
+  | Turtle Object
+
+type alias Object =
+  {
+    pos : Vec2
+  }
+
+type alias Vec2 =
+  { x : Float, y : Float }
+
 
 -------------- Input methods
 
@@ -50,8 +87,7 @@ type alias Input =
   , delta: Time }
 
 delta : Signal Time
-delta = Signal.map inSeconds (fps 15)
-
+delta = Signal.map inSeconds (fps 25)
 
 screenToWorld : (Int, Int) -> (Int, Int) -> (Float, Float)
 screenToWorld (width, height) (x, y) =
