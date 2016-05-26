37318e66579cb91a8a38f252f3d6e8d5daf13122
Wil Chung <iamwil@gmail.com>
2016-05-25 17:09:23 -0700

    add isRole to collidable

--- src/component/collidable.elm
+++ src/component/collidable.elm
@@ -10,3 +10,10 @@ init : Entity.Role.Name -> Model
 init role = {
     role = role
   }
+
+isRole : Entity.Role.Name -> Model -> Maybe Model
+isRole role coll =
+  if coll.role == role then
+    Just coll
+  else
+    Nothing
